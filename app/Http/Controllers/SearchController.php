e6ad6552d682e482b0407798fb1449d8dd0c6763
kk0055 <rtsw.lgy@gmail.com>
2023-10-04 15:21:57 +0900

    API接続

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
+
+class SearchController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $apiKey = env('API_KEY');
+        $id = env('SEARCH_ENGINE_ID');
+        $query = request()->query('word');
+        $query = '雪平莉左';
+        $url = "https://www.googleapis.com/customsearch/v1?key=$apiKey&cx=$id&q=$query";
+        $response = Http::get($url);
+        return $response;
+    }
+}
